a2cf1096b141875a45e47569a537fcdcbabdc111
Antonio Pardo <apardo@alabs.es>
2012-02-26 08:26:56 +0100

    primer commit para dominar el mundo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alabs::Application.config.secret_token = 'd0177a7f1015ef235a142a6022242afa1771d6e1f94a28024b111ba8c638fd47e18696c6e9045dd47e717a74887e3b15e8868abc7a612de52cb01c3e01aa7943'
