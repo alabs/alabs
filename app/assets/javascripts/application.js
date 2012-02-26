3c71f2fb59e5dc3503f61e6f8a59f54dd29604b4
Antonio Pardo <apardo@alabs.es>
2012-02-26 15:58:01 +0100

    terminado prototipo home

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,32 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.phono
 //= require_tree .
+
+$(document).ready(function() {
+
+  // para el click2call
+  var phono = $.phono({
+    apiKey: "60d309ac89b78f74a49955d8ba3a9d6f",
+    onReady: function() {
+      $("#call").attr("disabled", false).val("Llamar a aLabs");
+    }
+  });
+
+  $("#call").click(function() {
+    $("#call").attr("disabled", true).val("Llamada en curso");
+    phono.phone.dial("sip:2000@alabs.es:5070", {
+      onRing: function() {
+        $("#status").html("Llamando");
+      },
+      onAnswer: function() {
+        $("#status").html("Hablando");
+      },
+      onHangup: function() {
+        $("#call").attr("disabled", false).val("Llamar a aLabs");
+        $("#status").html("Colgar");
+      }
+    });
+  });
+});
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,22 @@
 class PagesController < ApplicationController
 
   def index
+    # Para pillar los titulares del blog
+    require 'rss'
+    rss_feed = "http://feeds.feedburner.com/alabs/blog"
+    rss = RSS::Parser.parse(rss_feed, false)
+    @posts = []
+    rss.items.each_with_index do |i, n|
+      break if n == 4
+      item = { 
+        'link' => i.link,
+        'title' => i.title,
+      }
+      @posts << item
+    end
+    @posts
+
+    # Mostrar los últimos tweets
+    @tweets = Twitter.user_timeline('alabs_es')[0,4]
   end
 end
