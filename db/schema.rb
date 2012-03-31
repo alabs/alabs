055b94d99cacf737b4f3698caee5c75e83414f10
Antonio Pardo <apardo@alabs.es>
2012-03-31 12:10:07 +0200

    nav_link method

--- db/schema.rb
+++ db/schema.rb
@@ -23,4 +23,14 @@ ActiveRecord::Schema.define(:version => 20120226205729) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
 end
